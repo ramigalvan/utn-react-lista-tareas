0b92b2d6af3e70b35a047e40670a0545ad7602bd
ramirogalvan <rgalvan@asap-consulting.net>
2025-08-29 12:36:45 -0300

    feat: agregado css APp

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import type { RootState } from './store/store'
-import FormTarea from './components/FormTarea'
-import ListaTareas from './components/ListaTareas'
-// import './App.css'
+import FormTarea from './components/tarea/FormTarea';
+import ListaTareas from './components/tarea/ListaTareas';
+import styles from "./App.module.css"
 
 
 function App() {
   const [modoEdicion, setModoEdicion] = useState<string | null>(null)
   const tareas = useSelector((state: RootState) => state.tareas.lista);
-
   const tareaEdicion = modoEdicion ? tareas.find(t => t.id === modoEdicion) : null;
-  return (
-    <>
-      <h1>Lista de Tareas - Redux</h1>
-      <FormTarea
-        modoEdicion={modoEdicion}
-        setModoEdicion={setModoEdicion}
-        tareaEdicion={tareaEdicion ?? undefined}
-      />
 
-      <ListaTareas onEditar={setModoEdicion} />
-    </>
-  )
+  return (
+    <div className={styles.app}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Lista de Tareas - Redux</h1>
+        <FormTarea
+          modoEdicion={modoEdicion}
+          setModoEdicion={setModoEdicion}
+          tareaEdicion={tareaEdicion ?? undefined}
+        />
+        <ListaTareas onEditar={setModoEdicion} />
+      </main>
+    </div>
+  );
 }
 
 export default App
